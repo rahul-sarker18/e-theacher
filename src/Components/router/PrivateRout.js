6f1cf6226a89d6d30940734b13cd270ea6ce212f
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-11-08 02:19:04 +0530

    private router sum work

--- src/Components/router/PrivateRout.js
+++ src/Components/router/PrivateRout.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../Context/Usercontext";
 
-const PrivateRout = () => {
+const PrivateRout = ({ children }) => {
+  const { user, loder } = useContext(AuthContext);
+
+  if (loder) {
     return (
-        <div>
-            
+      <div className="flex flex-col mx-auto my-20 m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+        <div className="h-48 rounded-t dark:bg-gray-700"></div>
+        <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-900">
+          <div className="w-full h-6 rounded dark:bg-gray-700"></div>
+          <div className="w-full h-6 rounded dark:bg-gray-700"></div>
+          <div className="w-3/4 h-6 rounded dark:bg-gray-700"></div>
         </div>
+      </div>
     );
+  }
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login"></Navigate>;
 };
 
 export default PrivateRout;
